3470c905dc3e2c5cd2809779719aa2e2235734ab
Jun-Qian <559924jq@eur.nl>
2026-07-31 16:47:32 +0800

    Simplify workflow validation table

--- prototypes/kisoku-nintei/tools/build-prd.mjs
+++ prototypes/kisoku-nintei/tools/build-prd.mjs
@@ -22,7 +22,6 @@ function inlineMarkdown(text) {
   out = out.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
   out = out.replace(/&lt;(ins|mark|del) class=&quot;(change-(?:added|modified|deleted))&quot;&gt;/g, '<$1 class="$2">');
   out = out.replace(/&lt;\/(ins|mark|del)&gt;/g, '</$1>');
-  out = out.replace(/&lt;br\s*\/?&gt;/g, '<br />');
   return out;
 }
 
